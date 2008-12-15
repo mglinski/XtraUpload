d0aac4623dc06e1494f849d149f05e165b5222af
matthewglinski <matthewglinski@0aae1ab1-9915-d04d-beca-595075e595f9>
2008-12-15 18:06:36 +0000

    Fixed Upgrade code error
    git-svn-id: http://subversion.assembla.com/svn/xtraupload/trunk@141 0aae1ab1-9915-d04d-beca-595075e595f9

--- upgrade/includes/sql/sql_2.0.0,0.0.1.0.php
+++ upgrade/includes/sql/sql_2.0.0,0.0.1.0.php
@@ -4,5 +4,5 @@
 $data = array('id' =>  NULL, 'name' => 'show_recent_uploads', 'value' => '1', 'description1' => 'Show Recent Uploads', 'description2' => 'Yes|-|No<br /><br />Show a list of the 5 most recently uploaded files?', 'group' => 0, 'type' => 'yesno', 'invincible' => 1);
 $this->db->insert('config', $data);
 
-$data = array('value' => '2.0.0,0.2.0.0');
+$data = array('value' => '2.0.0,0.0.2.0');
 $this->db->where('name', '_db_version')->update('config', $data);
