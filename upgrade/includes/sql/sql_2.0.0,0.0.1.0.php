575a9f999131ca8e79db0ae1d1b58daf02134951
matthewglinski <matthewglinski@0aae1ab1-9915-d04d-beca-595075e595f9>
2009-01-30 18:04:39 +0000

    git-svn-id: http://subversion.assembla.com/svn/xtraupload/trunk@153 0aae1ab1-9915-d04d-beca-595075e595f9

--- upgrade/includes/sql/sql_2.0.0,0.0.1.0.php
+++ upgrade/includes/sql/sql_2.0.0,0.0.1.0.php
@@ -1,8 +1,1 @@
-<?php  if ( ! defined("BASEPATH")) exit("No direct script access allowed");
-
-// 2.0.0 B1 -> 2.0.0 B2 | Upgrade Commands
-$data = array('id' =>  NULL, 'name' => 'show_recent_uploads', 'value' => '1', 'description1' => 'Show Recent Uploads', 'description2' => 'Yes|-|No<br /><br />Show a list of the 5 most recently uploaded files?', 'group' => 0, 'type' => 'yesno', 'invincible' => 1);
-$this->db->insert('config', $data);
-
-$data = array('value' => '2.0.0,0.0.2.0');
-$this->db->where('name', '_db_version')->update('config', $data);
+<?php  if ( ! defined("BASEPATH")) exit("No direct script access allowed");// 2.0.0 B1 -> 2.0.0 B2 | Upgrade Commands$data = array('id' =>  NULL, 'name' => 'show_recent_uploads', 'value' => '1', 'description1' => 'Show Recent Uploads', 'description2' => 'Yes|-|No<br /><br />Show a list of the 5 most recently uploaded files?', 'group' => 0, 'type' => 'yesno', 'invincible' => 1);$this->db->insert('config', $data);// B1 had no definate way to determine DB version, so if upgrading from B!, insert the _db_version entryif($this->db->get_where('config', array('name' => '_db_version'))->num_rows() == 0){	$data = array('id' =>  NULL, 'name' => '_db_version', 'value' => $this->_db_version, 'description1' => '', 'description2' => '', 'group' => 0, 'type' => 'text', 'invincible' => 1);	$this->db->insert('config', $data);}else{	$data = array('value' => '2.0.0,0.0.2.0');	$this->db->where('name', '_db_version')->update('config', $data);}
