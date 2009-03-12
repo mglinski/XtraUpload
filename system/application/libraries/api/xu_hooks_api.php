75ee11f4aff353a45403360642320e3be6cbda2e
matthewglinski <matthewglinski@0aae1ab1-9915-d04d-beca-595075e595f9>
2009-03-12 23:06:05 +0000

    resolved private user group display error
    git-svn-id: http://subversion.assembla.com/svn/xtraupload/trunk@193 0aae1ab1-9915-d04d-beca-595075e595f9

--- system/application/libraries/api/xu_hooks_api.php
+++ system/application/libraries/api/xu_hooks_api.php
@@ -1,90 +1,1 @@
-<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
-/**
- * XtraUpload
- *
- * A turn-key open source web 2.0 PHP file uploading package requiring PHP v5
- *
- * @package		XtraUpload
- * @author		Matthew Glinski
- * @copyright	Copyright (c) 2006, XtraFile.com
- * @license		http://xtrafile.com/docs/license
- * @link		http://xtrafile.com
- * @since		Version 2.0
- * @filesource
- */
-
-// ------------------------------------------------------------------------
-
-/**
- * XtraUpload XU_API Hooks Library
- *
- * @package		XtraUpload
- * @subpackage	Library
- * @category	Library
- * @author		Matthew Glinski
- * @link		http://xtrafile.com/docs/api/hooks
- */
-
-// ------------------------------------------------------------------------
-
-class Xu_hooks_api
-{
-	private $store;
-	private $CI;
-	
-	function Xu_hooks_api()
-	{
-		$this->CI =& get_instance();
-		log_message('debug', "XtraUpload Hooks API Class Initialized");
-		$this->init();
-	}
-	
-	private function init()
-	{
-		$this->store = new stdClass();
-		$this->store->hooks = array();
-	}
-	
-	// Create API hook
-	function setHook($name, $functionName, $sid)
-	{
-		if(!isset($this->store->hooks[$name]))
-		{
-			$this->store->hooks[$name] = array();
-		}
-		$this->store->hooks[$name][$sid] = $functionName;
-	}
-	
-	// Delete your API hook
-	function deleteHook($name, $sid)
-	{
-		if(!isset($this->store->hooks[$name]))
-		{
-			return false;
-		}
-		unset($this->store->hooks[$name][$sid]);
-	}
-	
-	// [SYSTEM] -> run hooks for a function
-	function _runHook($name, $vars=array())
-	{
-		if(!isset($this->store->hooks[$name]))
-		{
-			return false;
-		}
-		
-		foreach($this->store->hooks[$name] as $k => $function)
-		{
-			$function($vars);
-		}
-		
-		return $vars;
-	}
-	
-	// [SYSTEM] -> return the raw hooks store variable
-	function _getStore($item='hooks')
-	{
-		return $this->store->$item;
-	}
-}
-?>
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');/** * XtraUpload * * A turn-key open source web 2.0 PHP file uploading package requiring PHP v5 * * @package		XtraUpload * @author		Matthew Glinski * @copyright	Copyright (c) 2006, XtraFile.com * @license		http://xtrafile.com/docs/license * @link		http://xtrafile.com * @since		Version 2.0 * @filesource */// ------------------------------------------------------------------------/** * XtraUpload XU_API Hooks Library * * @package		XtraUpload * @subpackage	Library * @category	Library * @author		Matthew Glinski * @link		http://xtrafile.com/docs/api/hooks */// ------------------------------------------------------------------------class Xu_hooks_api{	private $CI = '';	private $hooks = array();		function Xu_hooks_api()	{		$this->CI =& get_instance();		log_message('debug', "XtraUpload Hooks API Class Initialized");		$this->init();	}		private function init()	{		return;	}		// set a hook to be run	public function setHook($name, $function)	{		if(!isset($this->hooks[$name]))		{			$this->hooks[$name] = array();		}				$this->hooks[$name][] = $function;	}		// Run all set hooks, and return the data	public function runHooks($name, $args)	{		if(!isset($this->hooks[$name]) or !is_array($this->hooks[$name]) or empty($this->hooks[$name]))		{			return $args;			}				// dirty work, needs to be rethought. I hate eval statments... 		foreach($this->hooks[$name] as $function)		{			$args = $function($args);		}		return $args;	}		// [SYSTEM] -> return the raw hooks store variable	public function _getHooks()	{		return $this->hooks;	}		// [SYSTEM] -> apply the raw hooks store variable	public function _putHooks($hooks)	{		$this->hooks = $hooks;	}}?>
