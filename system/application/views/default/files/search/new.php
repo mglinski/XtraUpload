c2be4feec8cd97e9812c7c1ced5af235dd52bd4d
matthewglinski <matthewglinski@0aae1ab1-9915-d04d-beca-595075e595f9>
2009-03-12 01:10:09 +0000

    added new group options changed logic for admin panel link optimized some code
    git-svn-id: http://subversion.assembla.com/svn/xtraupload/trunk@188 0aae1ab1-9915-d04d-beca-595075e595f9

--- system/application/views/default/files/search/new.php
+++ system/application/views/default/files/search/new.php
@@ -1,20 +1,1 @@
-<h2 style="vertical-align:middle"><img src="<?=base_url().'img/icons/search_32.png'?>" class="nb" alt="" /> Search Files</h2>
-<?=$flashMessage?>
-
-<form action="<?=site_url('files/search')?>" onsubmit="submitSearch(); return false;"  method="get">
-	<p>
-		<label>Search Text(can be file id, part of a file name, or part of a file description)</label>
-		<input type="text" name="query" id="search" value="" /><br /><br />
-		
-		<?=generateLinkButton('Search Files', 'javascript:;', base_url().'img/icons/search_16.png', NULL, array('onclick' => 'submitSearch();'))?><br />
-	</p>
-</form>
-<script>
-function submitSearch()
-{
-	if($('#search').val() != '')
-	{
-		window.location = '<?=site_url('files/search')?>/'+escape($('#search').val());
-	}
-}
-</script>
+<?if(!$this->startup->group_config->can_search){	?>	<h2 style="vertical-align:middle"><img src="<?=base_url().'img/icons/search_32.png'?>" class="nb" alt="" /> Search Files</h2>	<span class="alert">You are currently not allowed to search files. Please <a href="<?=site_url('user/login')?>">login</a> to gain access.</span>	<?}else{	?>	<h2 style="vertical-align:middle"><img src="<?=base_url().'img/icons/search_32.png'?>" class="nb" alt="" /> Search Files</h2>	<?=$flashMessage?>		<form action="<?=site_url('files/search')?>" onsubmit="submitSearch(); return false;"  method="get">		<p>			<label>Search Text(can be file id, part of a file name, or part of a file description)</label>			<input type="text" name="query" id="search" value="" /><br /><br />						<?=generateLinkButton('Search Files', 'javascript:;', base_url().'img/icons/search_16.png', NULL, array('onclick' => 'submitSearch();'))?><br />		</p>	</form>	<script>	function submitSearch()	{		if($('#search').val() != '')		{			window.location = '<?=site_url('files/search')?>/'+escape($('#search').val());		}	}	</script>	<?}
--- system/application/views/default/upload/url.php
+++ system/application/views/default/upload/url.php
@@ -1,109 +1,1 @@
-<h2 style="vertical-align:middle"><img src="<?php echo base_url().'img/icons/connect_32.png'?>" class="nb" alt="" /><?php echo $this->lang->line('upload_url_header')?></h2>
-<p><?php echo $this->lang->line('upload_url_1')?><span style="color:#FF0000"><?php echo $this->lang->line('upload_url_2')?></span><?php echo $this->lang->line('upload_url_3')?><br />
-	<br />
-</p>
-
-<p>
-	<label for="linkBlock">
-		<a href="javascript:;" style="cursor:pointer" onclick="toggleUploadBlock()">
-			<img alt="" id="uploadImgSwitch" src="<?php echo $base_url?>img/other/remove_16.png" class="nb"/> 
-			<?php echo $this->lang->line('upload_url_4')?>
-		</a>
-	</label>
-	<span id="uploadTextBlock">
-		<textarea id="linkBlock" name="linkBlock" cols="65" rows="15"></textarea><br />
-		<span class="cssButton" style="display:block">
-			<a onclick="addToQueue();" class="buttonGreen" href="javascript:;"> 
-				<img alt="" src="<?php echo $base_url?>img/icons/add_16.png"/><?php echo $this->lang->line('upload_url_5')?>
-			</a>
-		</span><br />
-	</span>
-</p>
-
-
-<div id="files" style="display:">
-	<h3><?php echo $this->lang->line('upload_url_6')?></h3>
-	<div id="file_list">
-		<p>
-			<?php echo $this->lang->line('upload_url_7')?>(<span id="summary">0</span><?php echo $this->lang->line('upload_url_8')?>).<br />
-			<span class="alert" id="alert1" style="display:none"></span>
-			<span class="alert" id="alert2" style="display:none"></span>
-			<span class="float-right"><?php echo generateLinkButton($this->lang->line('upload_url_9'), 'javascript:startUploadQueue();', $base_url.'img/icons/up_16.png', 'green')?><br /><br /></span>
-		</p>
-		<div style="clear:both"></div>
-		<table border="0" style="width:98%" id="file_list_table">
-			<tr>
-				<th style="width:470px" class="align-left"><?php echo $this->lang->line('upload_url_10')?></th>
-				<th style="width:85px"><?php echo $this->lang->line('upload_url_11')?> <img title="<?php echo $this->lang->line('upload_url_12')?>" src="<?php echo $base_url?>img/icons/delete_16.png" onclick="clearUploadQueue()" alt="<?php echo $this->lang->line('upload_url_12')?>" style="cursor:pointer" class="nb" /></th>
-			</tr>
-		</table>
-		<p>
-			<span class="float-right"><?php echo generateLinkButton($this->lang->line('upload_url_9'), 'javascript:startUploadQueue();', $base_url.'img/icons/up_16.png', 'green')?><br /><br /></span>
-		</p>
-	</div>
-</div>
-
-<form action="<?=$server.'upload/url_process'?>" method="post" id="urlUp" style="display:none">
-	<p>
-		<input type="hidden" id="fid" name="fid" value="" />	
-		<input type="hidden" name="link" id="link" value="" />
-		<input type="hidden" name="descr" id="descr" value="" />
-		<input type="hidden" name="pass" id="pass" value="" />
-		<input type="hidden" name="user" id="pass" value="<?php echo $this->session->userdata('id')?>" />
-	</p>
-</form>
-
-<script type="text/javascript">
-	var doProgress = false;
-	var file_count = 0;
-	var filesToUpload = [];
-	var currentFileId;
-	var allowCheckProgress=false;
-	var fileIcons = new Array(<?php echo $file_icons?>);
-	
-	function toggleUploadBlock()
-	{
-		if($('#uploadTextBlock').css('display') != 'none')
-		{
-			$('#uploadTextBlock').slideUp('fast');
-			$('#uploadImgSwitch').attr('src', '<?php echo $base_url?>/img/icons/add_16.png');
-		}
-		else
-		{
-			$('#uploadTextBlock').slideDown('fast');
-			$('#uploadImgSwitch').attr('src', '<?php echo $base_url?>/img/other/remove_16.png');
-		}
-	}
-	
-	function ___getFileIcon(icon)
-	{
-		if(in_array(icon, fileIcons))
-		{
-			return icon;
-		}
-		else
-		{
-			return 'default';
-		}
-	}
-	
-	function in_array(needle, haystack, strict) 
-	{
-		// http://kevin.vanzonneveld.net
-		var found = false, key, strict = !!strict;
-	 
-		for (key in haystack) {
-			if ((strict && haystack[key] === needle) || (!strict && haystack[key] == needle)) {
-				found = true;
-				break;
-			}
-		}
-	 
-		return found;
-	}
-	
-	function ___progressURL()
-	{
-		return "<?php echo site_url('upload/get_progress')?>";
-	}
-</script>
+<?if(!$this->startup->group_config->can_url_upload){	?>	<h2 style="vertical-align:middle"><img src="<?php echo base_url().'img/icons/connect_32.png'?>" class="nb" alt="" /><?php echo $this->lang->line('upload_url_header')?></h2>	<span class="alert">You are currently not allowed to use URL Upload. Please <a href="<?=site_url('user/login')?>">login</a> to gain access.</span>	<?}else{?>	<h2 style="vertical-align:middle"><img src="<?php echo base_url().'img/icons/connect_32.png'?>" class="nb" alt="" /><?php echo $this->lang->line('upload_url_header')?></h2>	<p><?php echo $this->lang->line('upload_url_1')?><span style="color:#FF0000"><?php echo $this->lang->line('upload_url_2')?></span><?php echo $this->lang->line('upload_url_3')?><br />		<br />	</p>		<p>		<label for="linkBlock">			<a href="javascript:;" style="cursor:pointer" onclick="toggleUploadBlock()">				<img alt="" id="uploadImgSwitch" src="<?php echo $base_url?>img/other/remove_16.png" class="nb"/> 				<?php echo $this->lang->line('upload_url_4')?>			</a>		</label>		<span id="uploadTextBlock">			<textarea id="linkBlock" name="linkBlock" cols="65" rows="15"></textarea><br />			<span class="cssButton" style="display:block">				<a onclick="addToQueue();" class="buttonGreen" href="javascript:;"> 					<img alt="" src="<?php echo $base_url?>img/icons/add_16.png"/><?php echo $this->lang->line('upload_url_5')?>				</a>			</span><br />		</span>	</p>			<div id="files" style="display:">		<h3><?php echo $this->lang->line('upload_url_6')?></h3>		<div id="file_list">			<p>				<?php echo $this->lang->line('upload_url_7')?>(<span id="summary">0</span><?php echo $this->lang->line('upload_url_8')?>).<br />				<span class="alert" id="alert1" style="display:none"></span>				<span class="alert" id="alert2" style="display:none"></span>				<span class="float-right"><?php echo generateLinkButton($this->lang->line('upload_url_9'), 'javascript:startUploadQueue();', $base_url.'img/icons/up_16.png', 'green')?><br /><br /></span>			</p>			<div style="clear:both"></div>			<table border="0" style="width:98%" id="file_list_table">				<tr>					<th style="width:470px" class="align-left"><?php echo $this->lang->line('upload_url_10')?></th>					<th style="width:85px"><?php echo $this->lang->line('upload_url_11')?> <img title="<?php echo $this->lang->line('upload_url_12')?>" src="<?php echo $base_url?>img/icons/delete_16.png" onclick="clearUploadQueue()" alt="<?php echo $this->lang->line('upload_url_12')?>" style="cursor:pointer" class="nb" /></th>				</tr>			</table>			<p>				<span class="float-right"><?php echo generateLinkButton($this->lang->line('upload_url_9'), 'javascript:startUploadQueue();', $base_url.'img/icons/up_16.png', 'green')?><br /><br /></span>			</p>		</div>	</div>		<form action="<?=$server.'upload/url_process'?>" method="post" id="urlUp" style="display:none">		<p>			<input type="hidden" id="fid" name="fid" value="" />				<input type="hidden" name="link" id="link" value="" />			<input type="hidden" name="descr" id="descr" value="" />			<input type="hidden" name="pass" id="pass" value="" />			<input type="hidden" name="user" id="pass" value="<?php echo $this->session->userdata('id')?>" />		</p>	</form>		<script type="text/javascript">		var doProgress = false;		var file_count = 0;		var filesToUpload = [];		var currentFileId;		var allowCheckProgress=false;		var fileIcons = new Array(<?php echo $file_icons?>);				function toggleUploadBlock()		{			if($('#uploadTextBlock').css('display') != 'none')			{				$('#uploadTextBlock').slideUp('fast');				$('#uploadImgSwitch').attr('src', '<?php echo $base_url?>/img/icons/add_16.png');			}			else			{				$('#uploadTextBlock').slideDown('fast');				$('#uploadImgSwitch').attr('src', '<?php echo $base_url?>/img/other/remove_16.png');			}		}				function ___getFileIcon(icon)		{			if(in_array(icon, fileIcons))			{				return icon;			}			else			{				return 'default';			}		}				function in_array(needle, haystack, strict) 		{			// http://kevin.vanzonneveld.net			var found = false, key, strict = !!strict;		 			for (key in haystack) {				if ((strict && haystack[key] === needle) || (!strict && haystack[key] == needle)) {					found = true;					break;				}			}		 			return found;		}				function ___progressURL()		{			return "<?php echo site_url('upload/get_progress')?>";		}	</script><?}
