313892b685b2314c62e624aef51595373b83e771
mglinski <mglinski@0aae1ab1-9915-d04d-beca-595075e595f9>
2009-06-18 18:57:16 +0000

    language file updates
    git-svn-id: http://subversion.assembla.com/svn/xtraupload/trunk@268 0aae1ab1-9915-d04d-beca-595075e595f9

---
+++ system/application/language/english/upload_failure_lang.php
@@ -0,0 +1,28 @@
+<?php
+
+$lang['upload_fail_userfile_not_set'] = "Unable to find a post variable called userfile.";
+$lang['upload_fail_file_exceeds_limit'] = "The uploaded file exceeds the maximum allowed size in your PHP configuration file.";
+$lang['upload_fail_file_exceeds_form_limit'] = "The uploaded file exceeds the maximum size allowed by the submission form.";
+$lang['upload_fail_file_partial'] = "The file was only partially uploaded.";
+$lang['upload_fail_no_temp_directory'] = "The temporary folder is missing.";
+$lang['upload_fail_unable_to_write_file'] = "The file could not be written to disk.";
+$lang['upload_fail_stopped_by_extension'] = "The file upload was stopped by extension.";
+$lang['upload_fail_no_file_selected'] = "You did not select a file to upload.";
+$lang['upload_fail_invalid_filetype'] = "The filetype you are attempting to upload is not allowed.";
+$lang['upload_fail_invalid_filesize'] = "The file you are attempting to upload is larger than the permitted size.";
+$lang['upload_fail_invalid_dimensions'] = "The image you are attempting to upload exceedes the maximum height or width.";
+$lang['upload_fail_destination_error'] = "A problem was encountered while attempting to move the uploaded file to the final destination.";
+$lang['upload_fail_no_filepath'] = "The upload path does not appear to be valid.";
+$lang['upload_fail_no_file_types'] = "You have not specified any allowed file types.";
+$lang['upload_fail_bad_filename'] = "The file name you submitted already exists on the server.";
+$lang['upload_fail_not_writable'] = "The upload destination folder does not appear to be writable.";
+
+$lang['upload_fail_banned'] = "The file you uploaded is banned.";
+$lang['upload_fail_storage'] = "The file you uploaded is to large for your remaining storage space.";
+$lang['upload_fail_unknown'] = "The file you uploaded could not be saved for an unknown reason.";
+
+//upload_fail_
+
+
+/* End of file upload_fail_lang.php */
+/* Location: ./system/language/english/upload_fail_lang.php */
