c81c600090d244b3aec854b10facfcc86186ecb6
coded caffeine <coded caffeine@0aae1ab1-9915-d04d-beca-595075e595f9>
2009-07-03 10:42:53 +0000

    Updated language lines for admin/home view.
    git-svn-id: http://subversion.assembla.com/svn/xtraupload/trunk@270 0aae1ab1-9915-d04d-beca-595075e595f9

--- system/application/language/english/admin/home_lang.php
+++ system/application/language/english/admin/home_lang.php
@@ -27,3 +27,51 @@
 
 // ------------------------------------------------------------------------
 
+$lang['admin_home'] = 'Admin Home';
+$lang['admin_important_server_settings'] = 'Important Server Settings';
+$lang['admin_stats'] = 'XtraUpload v2 Stats';
+$lang['admin_server_stats'] = 'Server Stats';
+
+$lang['admin_upload_max_filesize_title'] = 'Max File Size';
+$lang['admin_post_max_size_title'] = 'Max POST-Request Size';
+$lang['admin_max_execution_time_title'] = 'Execution Timeout';
+$lang['admin_max_input_time_title'] = 'Request Input Timeout';
+$lang['admin_memory_limit_title'] = 'Memory Limit';
+$lang['admin_short_open_tag_title'] = 'Allow PHP Short Tags';
+
+$lang['admin_upload_max_filesize_description'] = 'The largest uploaded file size this server is configured to process is <em>{$}B</em>. <br />This overrides your group settings.';
+$lang['admin_post_max_size_description'] = 'The maximum size of all allowed POST data is <em>{$}B</em>';
+$lang['admin_max_execution_time_description'] = 'The longest amount of time your server can execute PHP files is <em>{$} second(s)</em>';
+$lang['admin_max_input_time_description'] = 'The longest request your server will process is <em>{$} second(s</em>)';
+$lang['admin_memory_limit_description'] = 'The maximum amount of memory a PHP script can use is <em>{$}B</em>';
+$lang['admin_short_open_tag_description'] = 'Enable the use of PHP short opening tags "&lt;?": {$}';
+
+$lang['admin_on'] = 'On';
+$lang['admin_off'] = 'Off';
+$lang['admin_seconds'] = 'second(s)';
+$lang['admin_recommended'] = 'Recommended';
+
+$lang['admin_settings_alert'] = 'Some of the above Settings are not ideal, please inspect these to ensure minimal problems.';
+$lang['admin_upgrade_available'] = 'Upgrade Available';
+$lang['admin_important_upgrade_available'] = 'Important Upgrade Available';
+$lang['admin_update_too'] = 'admin_important_upgrade_available';
+
+$lang['admin_number_of_uploads'] = 'Number of Uploads';
+$lang['admin_total_disk_space_used'] = 'Total Disk Space Used';
+$lang['admin_number_of_registered_users'] = 'Number of Registered Users';
+$lang['admin_total_bandwidth_used'] = 'Total Bandwidth Used';
+$lang['admin_number_of_admins'] = 'Number of Admins';
+$lang['admin_number_of_active_servers'] = 'Number of Active Servers';
+
+$lang['admin_server_load'] = 'Server Load';
+$lang['admin_total_disk_space'] = 'Total Disk Space';
+
+$lang['admin_used'] = 'Used';
+$lang['admin_free'] = 'Free';
+$lang['admin_useful_information'] = 'Useful Information';
+$lang['admin_you_are_using_the'] = 'You are using the';
+
+$lang['admin_this_is_xu_version'] = 'This is XtraUpload version';
+$lang['admin_skin'] = 'skin';
+$lang['admin_with'] = 'with';
+$lang['admin_plugins'] = 'plugins';
